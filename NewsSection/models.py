37ad6dcfd39355ba61b9fc85eb9414f489f3f69d
Ivan Koev <62701053+ivvlko@users.noreply.github.com>
2020-11-27 00:39:23 +0200

    Update models.py

--- NewsSection/models.py
+++ NewsSection/models.py
@@ -6,37 +6,23 @@ from NewsSection.validators import only_english_validator
 from django.db import models
 import django.utils.timezone
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ed763f4e7ea342aa845a41113142fd3e0c6c3a0
 
 class News(models.Model):
     choices = [['Computer Vision and Images', 'Computer Vision and Images'],
                ['Natural Language Processing', 'Natural Language Processing'],
                ['Robotics', 'Robotics'],
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ed763f4e7ea342aa845a41113142fd3e0c6c3a0
                ]
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, validators=[only_english_validator])
     text = models.TextField(max_length=3000, validators=[only_english_validator, MinLengthValidator(100)])
-<<<<<<< HEAD
 
-=======
->>>>>>> 8ed763f4e7ea342aa845a41113142fd3e0c6c3a0
                ['Hardware and Innovations', 'Hardware and Innovations']
                ]
 
     title = models.CharField(max_length=50)
     text = models.TextField(max_length=3000)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-<<<<<<< HEAD
 
-=======
->>>>>>> 8ed763f4e7ea342aa845a41113142fd3e0c6c3a0
     category = models.CharField(max_length=50, choices=choices)
     img_url = models.URLField()
     date_posted = models.DateTimeField(default=django.utils.timezone.now)
@@ -45,10 +31,6 @@ class News(models.Model):
         return f'{self.title} by {self.author} posted on {self.date_posted}'
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ed763f4e7ea342aa845a41113142fd3e0c6c3a0
 class Comment(models.Model):
     name = models.CharField(max_length=50, validators=[only_english_validator])
     comment = models.TextField(max_length=500, validators=[only_english_validator])
@@ -57,7 +39,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'By {self.name} on {self.date_posted}'
-<<<<<<< HEAD
 
-=======
->>>>>>> 8ed763f4e7ea342aa845a41113142fd3e0c6c3a0
