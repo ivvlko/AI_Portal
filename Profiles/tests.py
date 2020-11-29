82b33093c540a02168f4a0278bfad7df1cd73eeb
ivvlko <ivankoevbg@gmail.com>
2020-11-30 00:32:52 +0200

    fixing navbar, css and few tests

--- Profiles/tests.py
+++ Profiles/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from Profiles.forms import CustomRegisterForm
+
+
+class TestForms(TestCase):
+
+    def test_custom_register_form_with_correct_value_should_be_valid(self):
+        data = {'username': 'ivan', 'email': 'ivan@abv,bg', 'password1': '159753qwe', 'password2': '159753qwe'}
+        form = CustomRegisterForm(data)
+        self.assertTrue(form.is_valid())
+
+    def test_custom_register_form_with_invalid_pass_should_not_return(self):
+        data = {'username': 'ivan', 'email': 'ivan@abv,bg', 'password1': '159753qwe', 'password2': '159753qwe'}
+        form = CustomRegisterForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_custom_register_form_with_similar_username_and_pass_should_not_return(self):
+        data = {'username': 'ivan', 'email': 'ivan@abv,bg', 'password1': 'ivan', 'password2': 'ivan'}
+        form = CustomRegisterForm(data)
+        self.assertFalse(form.is_valid())
+
